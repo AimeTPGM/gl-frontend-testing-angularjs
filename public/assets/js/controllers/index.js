1f77611c3efd11cad6909d6c9f9de5d2b81cfe59
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-14 01:15:28 +0700

    add content

--- public/assets/js/controllers/index.js
+++ public/assets/js/controllers/index.js
@@ -8,4 +8,39 @@ angular.module('app')
 			"footer" : "/views/footer.html"
 		}
 	}
+
+	$scope.cards = [
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			},
+			{
+				"header" : "Lorem ipsum",
+				"content" : "dolor sit amet, consectetur adipiscing elit. Nullam sed nisi nec lorem viverra laoreet."
+			}
+		]
 ])
